5c065a9ce803c394438c2f982ccba9b6bd46a6db
Vladimir Arshba <hwarzz@yandex.ru>
2024-08-14 00:05:00 +0300

    14.08 00:05

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -10,9 +10,14 @@ def main():
     for i in range(1, 4):
         random_number = str(random.randint(1, 100))
         print(f'Question: {random_number}')
-        answer = prompt.string(random_number)
+        #answer = prompt.string(random_number)
+        answer = prompt.string(' ')
         print(f'Your answer: {answer}')
-
+        print(int(random_number)%2)
+        print(answer)
+        if int(random_number)%2 == 0 and answer == 'yes':
+             print('Correct!')
+        print('CCorrect') 
 
 if __name__ == '__main__':
     main()
