25fb6abd9fea807aa6171926875710b4f2e2e18b
Vladimir Arshba <hwarzz@yandex.ru>
2024-08-15 02:09:47 +0300

    15.08 - 02:10

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,19 +5,28 @@ from brain_games.cli import welcome_user
 
 
 def main():
-    welcome_user()
+    # welcome_user()
+    user_name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     for i in range(1, 4):
         random_number = str(random.randint(1, 100))
         print(f'Question: {random_number}')
-        #answer = prompt.string(random_number)
-        answer = prompt.string(' ')
-        print(f'Your answer: {answer}')
-        print(int(random_number)%2)
-        print(answer)
-        if int(random_number)%2 == 0 and answer == 'yes':
-             print('Correct!')
-        print('CCorrect') 
+        answer = prompt.string('Your answer: ')
+        # print(f'Your answer: {answer}')
+        # print(int(random_number)%2)
+        # if int(random_number)%2 == 0 and answer == 'yes':
+        if int(random_number)%2 == 0:
+            correct_answer = 'yes' 
+        if int(random_number)%2 == 1:
+            correct_answer = 'no'
+        if answer == correct_answer:
+            print('Correct!')
+        if i == 3:
+            print(f'Congratulations, {user_name}')
+            break 
+        if answer != correct_answer:
+            print(f'\'{answer}\' is wrong answer ;(. Correct answer was \'{correct_answer}\'. Let\'s try again, {user_name}!')
+            break
 
 if __name__ == '__main__':
     main()
